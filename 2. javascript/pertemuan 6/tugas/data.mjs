b81eb7f3719140437d3648ac5f24e3bad9e49bf8
Anang Febryan <anangfebrian818@gmail.com>
2025-03-26 22:22:32 +0700

    Javascript

---
+++ 2. javascript/pertemuan 6/tugas/data.mjs
@@ -0,0 +1,14 @@
+const users = [
+    { nama: 'John', umur: 30, alamat: 'Jakarta', email: 'john@example.com' },
+    { nama: 'Jane', umur: 25, alamat: 'Bandung', email: 'jane@example.com' },
+    { nama: 'Bob', umur: 35, alamat: 'Surabaya', email: 'bob@example.com' },
+    { nama: 'Alice', umur: 28, alamat: 'Semarang', email: 'alice@example.com' },
+    { nama: 'Charlie', umur: 32, alamat: 'Yogyakarta', email: 'charlie@example.com' },
+    { nama: 'David', umur: 27, alamat: 'Denpasar', email: 'david@example.com' },
+    { nama: 'Eva', umur: 29, alamat: 'Makassar', email: 'eva@example.com' },
+    { nama: 'Frank', umur: 31, alamat: 'Bali', email: 'frank@example.com' },
+    { nama: 'Grace', umur: 26, alamat: 'Surabaya', email: 'grace@example.com' },
+    { nama: 'Henry', umur: 33, alamat: 'Jakarta', email: 'henry@example.com' }
+];
+  
+  export default users;  
